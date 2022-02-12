1982b3885c9f45c6db6c8922a7fb1301e5fc98f0
GGahagan27 <71200016+GGahagan27@users.noreply.github.com>
2022-02-11 19:19:28 -0800

    Created table that highlights extreme temperature and windspeeds

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 'use strict';
 
-let temperatures = [ 9, 3, -2, 5, 4, -3];
-let windspeeds = [0, 55, 14, 9, 47, 3];
+let temperatures = [ 100, 101, -30, -40, 55, 15];
+let windspeeds = [2, 7, 75, 55, 45, 13];
+
+let showButton = document.getElementById('show');
+
+showButton.addEventListener('click',showWeather);
+
+let tbody = document.getElementById('simpleTableBody');
+
+
+function showWeather(e) {
+    console.log('in the showWeather() function');
+
+    for (let i=0; i<temperatures.length; i++) {
+        console.log (i);
+        console.log (temperatures[i], windspeeds[i])
+
+        let tempClass ="";
+        let windClass ="";
+
+        if (temperatures[i] < 0) {
+            tempClass = "table-danger";  
+        }
+
+        if (windspeeds[i] >35) {
+            windClass = "table-danger";
+        }
+
+        tbody.innerHTML += `<tr><td class="${tempClass}">${temperatures[i]}</td><td class="${windClass}">${windspeeds[i]}</td</tr>`;
+
+    }
+
+
+}
+      
+
+
+
+
 
